9c863660a3506f58c70ed91f7d8ee39c25404d3c
Paweł Motyl <pawmot@gmail.com>
2016-07-18 19:10:26 +0200

    Added @Repository to AssetRepository to stop IntelliJ from reporting a wiring error.

--- dam-rest/src/main/java/com/pawmot/dam/rest/repositories/AssetRepository.java
+++ dam-rest/src/main/java/com/pawmot/dam/rest/repositories/AssetRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.repository.Repository;
 import java.util.List;
 import java.util.UUID;
 
+@org.springframework.stereotype.Repository
 public interface AssetRepository extends Repository<Asset, UUID> {
     Asset findById(UUID id);
 
